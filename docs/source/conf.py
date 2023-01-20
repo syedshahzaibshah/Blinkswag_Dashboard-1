e9ec5e76245a153e92a4e566d5704e212edec044
RevProgrammer <109289210+RevProgrammer@users.noreply.github.com>
2023-01-20 08:31:00 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Zoho Books Custom Function Documention'
+copyright = '2023 BlinkSwag'
+author = 'BlinkSwag'
 
 release = '0.1'
 version = '0.1.0'
